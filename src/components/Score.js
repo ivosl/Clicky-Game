162e7930c9192a5747fd5536fa354b61d35fd6ba
Ivo <ivol@Ivos-MacBook-Air.local>
2018-04-21 00:09:08 -0500

    working files

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/score.css";
 
 class Score extends React.Component {
 
@@ -9,6 +10,8 @@ class Score extends React.Component {
 render(){
     return (
         <div>
+            <div className="jumbotron">
+            <h1 class="display-4">Clicky Game</h1> </div>
             <div className= "navbar-nav">Current score : {this.props.score} | Top Score : {this.props.topscore}</div>
            
         </div>
